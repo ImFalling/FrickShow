31f46da5ea68d37711af65c8de24a58e13a6d9a2
ImFalling <jakobdoesnotapproveofschool@gmail.com>
2016-04-08 14:12:43 +0200

    fix

--- scripts/frickShow.js
+++ scripts/frickShow.js
@@ -55,9 +55,8 @@ function makeFrickShow(id, slides, init, intervaltime, debug){
       var temp = document.createElement("img");
 
       //Set image and CSS attributes
-      temp.setAttribute("src", ""+id+"/"+id+""+i.toString()+".png") ||
-      temp.setAttribute("src", ""+id+"/"+id+""+i.toString()+".jpeg") ||
-      temp.setAttribute("src", ""+id+"/"+id+""+i.toString()+".gif");
+      temp.setAttribute("src", ""+id+"/"+id+""+i.toString()+".png");
+
       temp.style.opacity = 0;
       temp.style.display = "none";
       temp.style.verticalAlign = "middle";
