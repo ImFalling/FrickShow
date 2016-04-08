8b4a083ca7cd66496e2fb784655774d51956a3da
ImFalling <jakobdoesnotapproveofschool@gmail.com>
2016-04-08 14:13:23 +0200

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,3 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-var mainSlide = makeFrickShow("mainSlide", 5, 1000, 5000, true);
+var mainSlide = makeFrickShow("mainSlide", 4, 1000, 5000, true);
 });
